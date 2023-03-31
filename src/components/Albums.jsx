b3a39f358f3a44eab201ae4ccd1fa02d6ec3a42d
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-31 18:21:23 +0300

    style albums page

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -35,9 +35,8 @@ function Albums() {
   console.log(albums);
 
   return (
-    <div className="p-4 my-5">
-      {/* <h1>{albums[0].artists[0].name}</h1> */}
-      {/* <h2>{albums[0].album_type}</h2> */}
+    <div className="p-8 my-5">
+      <h2 className="text-lg font-bold mb-8">Albums</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {albums.map((album) => (
           <div
@@ -47,19 +46,22 @@ function Albums() {
             <img
               src={album.images[0].url}
               alt={album.name}
-              className="w-full"
+              className="w-full h-52 object-cover"
             />
-            <div className="p-4">
+          <div className="flex flex-col">
+          <div className="p-4">
               <h2 className="text-lg font-bold">{album.name}</h2>
+              <h3 className="mb-8">{album.artists[0].name}</h3>
               <p className="text-gray-600">{album.release_date}</p>
               <p className="text-gray-600">{album.total_tracks} tracks</p>
             </div>
             <a
               href={album.external_urls.spotify}
-              className="bg-gray-500 w-full text-white flex justify-center items-center"
+              className="bg-gray-500 w-full p-3 text-white flex justify-center items-center"
             >
-              preview on spotify
+              Preview on Spotify
             </a>
+          </div>
           </div>
         ))}
       </div>
